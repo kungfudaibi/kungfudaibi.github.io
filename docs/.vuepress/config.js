bfd51efb8d448a7871e7f1141b87b6bc3403a9c2
initialice <186405897+initialice@users.noreply.github.com>
2024-10-27 02:53:34 +0800

    Feature: Add English version 	modified:   docs/.vuepress/config.js 	new file:   docs/en/AI.md 	new file:   docs/en/HPC.md 	new file:   docs/en/README.md 	new file:   docs/en/about.md 	new file:   docs/en/algorithm.md 	new file:   docs/en/getting-started.md 	new file:   docs/en/introduction.md 	new file:   docs/en/linux.md 	new file:   docs/en/network-security.md 	new file:   docs/en/recruitment.md 	new file:   docs/en/self-taught-guide.md

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,33 +5,68 @@ import { mdEnhancePlugin } from "vuepress-plugin-md-enhance"
 import { searchProPlugin } from "vuepress-plugin-search-pro"
 import { commentPlugin } from '@vuepress/plugin-comment'
 export default defineUserConfig({
-  lang: 'zh-CN',
-  title: '山西大学开源软件协会',
-  description: 'SXU Open Source Association',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: '山西大学开源软件协会',
+      description: 'SXU Open Source Association',
+    },
+    '/en/': {
+      lang: 'en-US',
+      title: 'SXUOSA',
+      description: 'SXU Open Source Association',
+    },
+  },
   theme: defaultTheme({
     logo: '/image/logo.png',
-    // 修改导航栏配置
-    navbar: [
-      { text: '前言', link: '/foreword.md' },
-      { text: '开始', link: '/start.md' },
-      { text: '自学指南', link: '/guide.md'},
-      { text: 'Linux', link: '/linux.md'},
-      { text: '超算', link: '/HPC.md'},
-      { text: '算法', link: '/algorithm.md'},
-      { text: '网络安全', link: '/network.md'},
-      { text: 'AI', link: '/AI.md'},
-      { text: '百团', link: '/zhaoxin.md'},
-      { 
-        text: 'blog', 
-        children:[
-          { text: 'zhujiechong', link: '/blog/zhujiechong.md'},
-          { text: 'thefirsts.md', link: '/blog/thefirsts.md'}
-        ]
+    locales: {
+      '/': {
+        selectLanguageName: '简体中文',
+        navbar: [
+          { text: '前言', link: '/foreword.md' },
+          { text: '开始', link: '/start.md' },
+          { text: '自学指南', link: '/guide.md'},
+          { text: 'Linux', link: '/linux.md'},
+          { text: '超算', link: '/HPC.md'},
+          { text: '算法', link: '/algorithm.md'},
+          { text: '网络安全', link: '/network.md'},
+          { text: 'AI', link: '/AI.md'},
+          { text: '百团', link: '/zhaoxin.md'},
+          { 
+            text: 'blog', 
+            children:[
+              { text: 'zhujiechong', link: '/blog/zhujiechong.md'},
+              { text: 'thefirsts.md', link: '/blog/thefirsts.md'}
+            ]
+          },
+          { text: '关于', link: '/about.md'},
+          { text: 'Github', link: 'https://github.com/kungfudaibi/kungfudaibi.github.io'},
+        ],
       },
-      { text: '关于', link: '/about.md'},
-      { text: 'Github', link: 'https://github.com/kungfudaibi/kungfudaibi.github.io'},
-    ],
-
+      '/en/': {
+        selectLanguageName: 'English',
+        navbar: [
+          { text: 'Introduction', link: '/en/introduction.md' },
+          { text: 'Getting Started', link: '/en/getting-started.md' },
+          { text: 'Self-taught Guide', link: '/en/self-taught-guide.md'},
+          { text: 'Linux', link: '/en/linux.md'},
+          { text: 'HPC', link: '/en/HPC.md'},
+          { text: 'Algorithm', link: '/en/algorithm.md'},
+          { text: 'Network Security', link: '/en/network-security.md'},
+          { text: 'AI', link: '/en/AI.md'},
+          { text: 'Recruitment', link: '/en/recruitment.md'},
+          { 
+            text: 'Blogs', 
+            children:[
+              { text: 'zhujiechong', link: '/en/blog/zhujiechong.md'},
+              { text: 'thefirsts', link: '/en/blog/thefirsts.md'}
+            ]
+          },
+          { text: 'About', link: '/en/about.md'},
+          { text: 'Github', link: 'https://github.com/kungfudaibi/kungfudaibi.github.io'},
+        ],
+      },
+    },
   }),
   plugins: [
     mdEnhancePlugin({
